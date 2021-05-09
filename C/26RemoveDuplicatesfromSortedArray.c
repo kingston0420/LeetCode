26c9348d8a65b3c68b8adb35734d98524b670152
Kingston <kingston880420@gmail.com>
2021-05-08 23:29:29 -0700

    Update 26RemoveDuplicatesfromSortedArray.c

--- C/26RemoveDuplicatesfromSortedArray.c
+++ C/26RemoveDuplicatesfromSortedArray.c
@@ -1,3 +1,4 @@
+/*
 Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
 
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
@@ -37,3 +38,22 @@ Constraints:
 0 <= nums.length <= 3 * 104
 -104 <= nums[i] <= 104
 nums is sorted in ascending order.
+*/
+
+
+
+int removeDuplicates(int* nums, int numsSize){
+    int j=0;
+    int finalSize = 0;
+    if (numsSize==0) {
+        return 0;
+    }
+    for (int i=0; i<numsSize; i++) {
+        if (nums[i]!=nums[j]) {
+            j++;
+            nums[j] = nums[i];
+            finalSize++;
+        }
+    }
+    return finalSize + 1;
+}
