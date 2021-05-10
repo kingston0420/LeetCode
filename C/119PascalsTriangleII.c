ec7dcc9070b6214bc830cc51d39ecc52fec2d6c2
Kingston <kingston880420@gmail.com>
2021-05-10 09:44:13 -0700

    Create 119PascalsTriangleII.c

---
+++ C/119PascalsTriangleII.c
@@ -0,0 +1,46 @@
+/*
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+ 
+
+Example 1:
+
+Input: rowIndex = 3
+Output: [1,3,3,1]
+Example 2:
+
+Input: rowIndex = 0
+Output: [1]
+Example 3:
+
+Input: rowIndex = 1
+Output: [1,1]
+ 
+
+Constraints:
+
+0 <= rowIndex <= 33
+ 
+
+Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
+*/
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getRow(int rowIndex, int* returnSize){
+    long temp;
+    int *output = (int*)malloc((rowIndex+1)*sizeof(int));
+    *returnSize = rowIndex + 1;
+    
+    output[0] = 1;
+    for (int i=0; i<rowIndex; i++) {
+        temp = output[i];
+        temp *= (rowIndex-i);
+        output[i+1] = temp/(i+1);
+    }
+    return output;
+}
