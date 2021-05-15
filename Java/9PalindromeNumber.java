d08294b928655968af5b55cf09dcb189399de807
Kingston <kingston880420@gmail.com>
2021-05-15 13:43:32 -0700

    Update 9PalindromeNumber.java

--- Java/9PalindromeNumber.java
+++ Java/9PalindromeNumber.java
@@ -1,23 +1,3 @@
-class Solution {
-    public boolean isPalindrome(int x) {
-        int digit = 0;
-        int reverse = 0;
-        if (x<0 || (x%10==0 && x!=0)) {
-            return false;
-        }
-        while (x>reverse) {
-            digit = x%10;
-            x = x/10;
-            reverse = reverse*10 + digit;
-        }
-        if (x == reverse || x==reverse/10) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-
 /*
 Given an integer x, return true if x is palindrome integer.
 
@@ -49,3 +29,26 @@ Constraints:
 
 -231 <= x <= 231 - 1
 */
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int digit = 0;
+        int reverse = 0;
+        if (x<0 || (x%10==0 && x!=0)) {
+            return false;
+        }
+        while (x>reverse) {
+            digit = x%10;
+            x = x/10;
+            reverse = reverse*10 + digit;
+        }
+        if (x == reverse || x==reverse/10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+//Time: O(N)
+//Space: O(1)
+
