1f4b0377196b1257816e9469bc509dc24c2142a1
Kingston <kingston880420@gmail.com>
2021-05-17 23:48:17 -0700

    Update 26RemoveDuplicatesFromSortedArray.java

--- Java/26RemoveDuplicatesFromSortedArray.java
+++ Java/26RemoveDuplicatesFromSortedArray.java
@@ -19,7 +19,7 @@ int len = removeDuplicates(nums);
 for (int i = 0; i < len; i++) {
     print(nums[i]);
 }
- 
+
 Example 1:
 
 Input: nums = [1,1,2]
@@ -31,7 +31,6 @@ Input: nums = [0,0,1,1,1,2,2,3,3,4]
 Output: 5, nums = [0,1,2,3,4]
 Explanation: Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively. It doesn't matter what values are set beyond the returned length.
  
-
 Constraints:
 
 0 <= nums.length <= 3 * 104
