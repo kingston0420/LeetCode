ccb5e38f750f0b26458e4a03261a02611ec01aa0
Kingston <kingston880420@gmail.com>
2021-05-26 11:52:55 -0700

    Create 832FlippinganImage.java

---
+++ Java/832FlippinganImage.java
@@ -0,0 +1,48 @@
+/*
+Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+
+To flip an image horizontally means that each row of the image is reversed.
+
+For example, flipping [1,1,0] horizontally results in [0,1,1].
+To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+
+For example, inverting [0,1,1] results in [1,0,0].
+ 
+
+Example 1:
+
+Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+Output: [[1,0,0],[0,1,0],[1,1,1]]
+Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+Example 2:
+
+Input: image = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ 
+
+Constraints:
+
+n == image.length
+n == image[i].length
+1 <= n <= 20
+images[i][j] is either 0 or 1.
+*/
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int width = image[0].length;
+        int height = image.length;
+        int[][] result = new int[height][width];
+        
+        for (int i=0; i<height; i++) {
+            for (int j=0; j<width; j++) {
+                result[i][j] = image[i][width-j-1]==0?1:0;
+            }
+        }
+        return result;
+    }
+}
+//Time: O(M*N) where M and N are the dimension of the array
+//Space: O(M*N)
